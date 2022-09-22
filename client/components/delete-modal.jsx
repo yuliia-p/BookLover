2812f0088bb233a38b23ba8b269cd0017b858baa
Yuliia Pchelintseva <junoromanova@gmail.com>
2022-09-22 03:24:43 +0000

    added req in index.js for deleting a book, updated app coponent and created one for modal + css for modal

--- client/components/delete-modal.jsx
+++ client/components/delete-modal.jsx
@@ -31,14 +31,19 @@ export default class DeleteModal extends React.Component {
 
   render() {
     return (
-      <div className='modal'>
+      <div className='modal delete-modal'>
           <div className="auth-modal-content">
             <i onClick={this.props.onClick} className="fa-solid fa-xmark"></i>
             <h1 className='no-margin '>BOOK<span className='header-lover'>LOVER</span></h1>
-            <h3 className='log-in-header'>Delete this book?</h3>
-            <div className="button-holder">
-            <button type="button" className="auth-button" onClick={this.props.onClick} >NO</button>
-            <button type="submit" className="auth-button" onClick={this.handleClick}>YES</button>
+            <h3 className='delete-header'>DELETE THIS BOOK?</h3>
+            <div className="button-holder y-n-holder flex">
+            <div className='flex-basis-100'>
+              <button type="submit" className="y-button" onClick={this.handleClick}>YES</button>
+              </div>
+            <div className='flex-basis-100'>
+              <button type="button" className="n-button" onClick={this.props.onClick} >NO</button>
+            </div>
+
             </div>
           </div>
       </div>
